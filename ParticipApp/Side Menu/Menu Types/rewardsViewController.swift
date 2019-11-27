1e6aa73620019b48e4fa6235742af7f4a9caef74
Gerardo Silva <gsilva.razo@gmail.com>
2019-11-26 18:27:03 -0600

    Added hardcoded rewards categories and horizontal view collection

--- ParticipApp/Side Menu/Menu Types/rewardsViewController.swift
+++ ParticipApp/Side Menu/Menu Types/rewardsViewController.swift
@@ -25,7 +25,7 @@ class rewardsViewController: UIViewController {
         Reward(title: "Reward1", description: "asdf", image: #imageLiteral(resourceName: "Recurso1")),
         Reward(title: "Reward1", description: "asdf", image: #imageLiteral(resourceName: "Recurso1")),
     ]
-    
+
     // Collection View for latestRewards
     fileprivate let latestRewardsCollectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
@@ -37,7 +37,7 @@ class rewardsViewController: UIViewController {
         cv.isUserInteractionEnabled = true
         return cv
     }()
-    
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -52,7 +52,7 @@ class rewardsViewController: UIViewController {
         latestRewardsCollectionView.heightAnchor.constraint(equalTo: latestRewardsCollectionView.widthAnchor, multiplier: 0.5).isActive = true
         latestRewardsCollectionView.delegate = self
         latestRewardsCollectionView.dataSource = self
-        
+
     }
     
     @IBAction func closePressed(_ sender: Any) {
@@ -77,34 +77,34 @@ class rewardsViewController: UIViewController {
 
 // Collection view extension
 extension rewardsViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
-    
+
     public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
     }
-    
+
     public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return data.count
     }
-    
+
     public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CustomCell
         cell.data = self.data[indexPath.row]
         return cell
     }
-    
-    
+
+
 }
 
 // Cell for collection view class
 class CustomCell: UICollectionViewCell {
-    
+
     var data: Reward? {
         didSet {
             guard let data = data else { return }
             bg.image = data.image
         }
     }
-    
+
     fileprivate let bg: UIImageView = {
         let iv = UIImageView()
         iv.image = #imageLiteral(resourceName: "Recurso1")
@@ -113,18 +113,18 @@ class CustomCell: UICollectionViewCell {
         iv.clipsToBounds = true
         return iv
     }()
-    
+
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
+
         contentView.addSubview(bg)
         bg.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
         bg.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
         bg.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
         bg.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
-        
+
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
